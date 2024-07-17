0922171c86462d8ec329fd0c06838cb5abdbc3fb
Adarsh tyagi <adarshtyagi.2003@gmail.com>
2024-07-17 18:06:53 +0530

    basic adding and find length

---
+++ add.py
@@ -0,0 +1,14 @@
+"""print(2+3)
+print('hello')
+name = input("name : ")
+print("my name is",name)
+
+age=int(input("age is: "))
+if (age>18):
+  print("vote")
+else:
+   print("no vote")"""
+
+name =input("user name:")
+print(name)
+print(len(name))
